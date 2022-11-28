b84ce0b81cc3eb6defc0ce18f31bd93ba733937a
w3villa-puja <puja.khandelwal@w3villa.com>
2022-11-28 12:52:01 +0530

    All codes push

---
+++ src/components/RelatedSongs.jsx
@@ -0,0 +1,22 @@
+import SongBar from './SongBar'
+
+const RelatedSongs = ({data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId}) => (
+  <div className='flex flex-col'>
+    <h1 className='font-bold text-3xl text-white'>Related Songs:</h1>
+    <div className='mt-6 w-full flex flex-col'>
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+          activeSong={activeSong}
+        />
+      ))}
+    </div>
+  </div>
+);
+export default RelatedSongs;
